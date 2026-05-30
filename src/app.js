da8e62b2201c8a884ba1debd2c1e5355f3bf5f32
argx-mchide <mchide@argenx.com>
2026-05-31 00:18:46 +0530

    first demo server creted

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+
+app.use("/test",(req,res) => {
+    res.send("Hello testing")
+})
+app.use("/mithun",(req,res) => {
+    res.send("Hey Mithun....enjoy coding")
+})
+app.use((req,res) => {
+    res.send("Hello Mithun")
+})
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
