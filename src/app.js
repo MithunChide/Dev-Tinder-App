a918fb1a665146152f5a8c89beb78ca95554423e
Mithun Chide <mithunchide03@gmail.com>
2026-06-12 01:07:49 +0530

    Get api with find all and fine one user

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const app = express();
 
 app.use(express.json()); // Add this middleware for JSON parsing
 
-
+//addn the data into database
 app.post("/signup", async (req, res) => {
    const user = new User(req.body);
-
    try {
    await user.save();
     res.send("User added successfully...")
@@ -17,6 +16,36 @@ app.post("/signup", async (req, res) => {
      console.log("Error:", err);
      res.status(400).send("Error saving the data:" + err.message)
    }
+});
+
+//Get the user using email from the database
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const user = await User.find({email: userEmail});
+        if(user.length === 0) {
+            res.status(404).send("User not found")
+        } else {
+            res.send(user);
+        }
+    } catch (err) { 
+        res.status(400).send("something went wrong")
+    }
+});
+
+//Get /feed all the users from the database 
+app.get("/feed", async (req,res) => {
+     try {
+        const users = await User.find({});
+        if(!users) {
+            res.status(404).send("User not found")
+        } else {
+            res.send(users);
+        }
+    } catch (err) { 
+        res.status(400).send("something went wrong")
+    }
+
 });
 
 
